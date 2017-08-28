12e76f170af96b517cb0b768e0c8b588c5de3c37
Luís Ferreira <lsferreira169@gmail.com>
2017-08-28 10:13:30 +0100

    Fix ambiguous library name

--- module.cmake
+++ module.cmake
@@ -6,8 +6,8 @@ endif()
 
 include_directories(${MODULE_CLI_DIR}/include)
 
-add_library (aurora-cli SHARED ${MODULE_CLI_SOURCE_DIR}/Color.cpp
+add_library (aurorafw-cli SHARED ${MODULE_CLI_SOURCE_DIR}/Color.cpp
                                ${MODULE_CLI_SOURCE_DIR}/Input.cpp
 						   	   ${MODULE_CLI_SOURCE_DIR}/Output.cpp)
 
-set_target_properties(aurora-cli PROPERTIES OUTPUT_NAME aurora-cli)
+set_target_properties(aurorafw-cli PROPERTIES OUTPUT_NAME aurorafw-cli)

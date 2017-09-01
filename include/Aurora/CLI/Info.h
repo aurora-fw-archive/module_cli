7071b3d56dc7b4cd2e701e54e7fd7aca0bde54be
Ev1lbl0w <ricasubtil@gmail.com>
2017-09-01 13:19:23 +0100

    Updated copyrigth and links

--- include/Aurora/CLI/Info.h
+++ include/Aurora/CLI/Info.h
@@ -1,8 +1,8 @@
 // ┌─┐┬ ┬┬─┐┌─┐┬─┐┌─┐  ┌─┐┬─┐┌─┐┌┬┐┌─┐┬ ┬┌─┐┬─┐┬┌─ | Powerful, Scalable and Cross Platform Framework
 // ├─┤│ │├┬┘│ │├┬┘├─┤  ├┤ ├┬┘├─┤│││├┤ ││││ │├┬┘├┴┐ | @author Luís Ferreira
 // ┴ ┴└─┘┴└─└─┘┴└─┴ ┴  └  ┴└─┴ ┴┴ ┴└─┘└┴┘└─┘┴└─┴ ┴ | @license GNU Public License v3
-//  Copyright (c) 2016 - Luís Ferreira. All right reserved
-//  More information in: https://github.com/ljmf00/ (Github Page)
+//  Copyright (c) 2017 - Luís Ferreira. All right reserved
+//  More information in: https://github.com/aurora-fw/ (Github Page)
 
 #include <Aurora/Lib/Target.h>
 #if defined(AURORA_TARGET_PRAGMA_ONCE_SUPPORT) && AURORA_TARGET_PRAGMA_ONCE_SUPPORT
